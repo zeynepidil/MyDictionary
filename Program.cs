02bc7f01dcffc6264bb30bbf8de64090d2a488f9
zeynepidil <77980443+zeynepidil@users.noreply.github.com>
2021-01-30 22:05:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            int fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] { };
+
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Product [] urunler = new Product [] { urun1, urun2 };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+
+
+    }
+            Console.WriteLine("......Metotlar....");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil Armut", 12,10);
+            sepetManager.Ekle2("Elma", "Yeşil elma", 12,9);
+            sepetManager.Ekle2("Karpuz","Diyarbakır Karpuzu",12,8);
+
+            
+        }
+    }
+}
+
+// don't repeat yourself
