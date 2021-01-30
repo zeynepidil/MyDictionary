9d3ce759619353b4f0d25a57a040aaf4d81e39bc
zeynepidil <77980443+zeynepidil@users.noreply.github.com>
2021-01-30 22:14:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> kıyafetler = new Dictionary<string, int>();
+            kıyafetler.Add("etek", 10);
+            Console.WriteLine("etek", 10);
+            kıyafetler.Add("elbise", 50);
+            Console.WriteLine("elbise", 50);
+            kıyafetler.Add("gömlek", 5);
+            Console.WriteLine("gömlek", 5);
+            Console.WriteLine(kıyafetler.Count);
+        }
+
+    }
+}
+    
